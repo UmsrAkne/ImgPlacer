4ff5c8de3b3e449ea4c58a2c74ecb5287e0942c8
UmsrAkne <sibata486@gmail.com>
2025-11-16 07:06:24 +0900

    feat: bind canvas dimensions to ViewModel and add centering offset calculation
    - Replaced hardcoded canvas dimensions in XAML with bindings to `CanvasWidth` and `CanvasHeight`.
    - Added `GetCenteredOffset` method to calculate centered offsets based on canvas and image dimensions.
    - Introduced `CanvasWidth` and `CanvasHeight` properties in `ImageCanvasViewerViewModel` for dynamic size handling.

--- ImgPlacer/ViewModels/ImageCanvasViewerViewModel.cs
+++ ImgPlacer/ViewModels/ImageCanvasViewerViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using Prism.Mvvm;
 
 namespace ImgPlacer.ViewModels
@@ -9,6 +10,10 @@ namespace ImgPlacer.ViewModels
         private double offsetX;
         private double offsetY;
         private double zoom = 1.0;
+        private double imageWidth;
+        private double imageHeight;
+        private double canvasHeight;
+        private double canvasWidth;
 
         public ObservableCollection<ImageListViewModel> Layers { get; set; }
 
@@ -17,5 +22,24 @@ namespace ImgPlacer.ViewModels
         public double OffsetY { get => offsetY; set => SetProperty(ref offsetY, value); }
 
         public double Zoom { get => zoom; set => SetProperty(ref zoom, value); }
+
+        public double ImageWidth { get => imageWidth; set => SetProperty(ref imageWidth, value); }
+
+        public double ImageHeight { get => imageHeight; set => SetProperty(ref imageHeight, value); }
+
+        public double CanvasHeight { get => canvasHeight; set => SetProperty(ref canvasHeight, value); }
+
+        public double CanvasWidth { get => canvasWidth; set => SetProperty(ref canvasWidth, value); }
+
+        public Point GetCenteredOffset()
+        {
+            var canvasCenter = new Point(CanvasWidth / 2.0, CanvasHeight / 2.0);
+            var imageCenter = new Point((ImageWidth * Zoom) / 2.0, (ImageHeight * Zoom) / 2.0);
+            var offset = new Point(OffsetX, OffsetY);
+
+            return new Point(
+                offset.X + imageCenter.X - canvasCenter.X,
+                offset.Y + imageCenter.Y - canvasCenter.Y);
+        }
     }
 }
