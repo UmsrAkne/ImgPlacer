5050bd22c9980ea652bfda87c607bcfbcbe9f124
UmsrAkne <sibata486@gmail.com>
2025-11-25 18:44:07 +0900

    feat(xml-editor): add attribute editor panel and template selector for dynamic UI

---
+++ ImgPlacer/Views/Selectors/AttributeEditorTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using ImgPlacer.ViewModels.Xml;
+
+namespace ImgPlacer.Views.Selectors
+{
+    public class AttributeEditorTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TextTemplate { get; set; }
+
+        public DataTemplate BoolTemplate { get; set; }
+
+        public DataTemplate IntTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var attr = (XmlAttributeViewModel)item;
+
+            if (attr == null)
+            {
+                return TextTemplate;
+            }
+
+            if (attr.ValueType == typeof(bool))
+            {
+                return BoolTemplate;
+            }
+
+            if (attr.ValueType == typeof(int))
+            {
+                return IntTemplate;
+            }
+
+            return TextTemplate;
+        }
+    }
+}
