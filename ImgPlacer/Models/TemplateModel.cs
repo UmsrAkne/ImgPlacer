bcd4d18a65ea3a3316387966cad035c3cfe15cab
UmsrAkne <sibata486@gmail.com>
2025-12-04 01:17:08 +0900

    feat(models): add TemplateModel with context mapping and tests
    - Introduced `TemplateModel` to map context data from `ToolPanelContext`.
    - Implemented `ToTemplateModel` method for converting layer and viewer details.
    - Added unit tests in `TemplateModelTests` to validate mapping logic.

---
+++ ImgPlacer/Models/TemplateModel.cs
@@ -0,0 +1,36 @@
+﻿using ImgPlacer.ViewModels;
+
+namespace ImgPlacer.Models
+{
+    public class TemplateModel
+    {
+        public string A { get; set; }
+
+        public string B { get; set; }
+
+        public string C { get; set; }
+
+        public string D { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public double Scale { get; set; }
+
+        public static TemplateModel ToTemplateModel(ToolPanelContext panelContext)
+        {
+            var model = new TemplateModel();
+            var layers = panelContext.ImageLayerManagerViewModel.Layers;
+            model.A = layers[0].SelectedImage?.FileName;
+            model.B = layers[1].SelectedImage?.FileName;
+            model.C = layers[2].SelectedImage?.FileName;
+            model.D = layers[3].SelectedImage?.FileName;
+            model.X = (int)panelContext.ImageCanvasViewerViewModel.GetCenteredOffset().X;
+            model.Y = (int)panelContext.ImageCanvasViewerViewModel.GetCenteredOffset().Y;
+            model.Scale = panelContext.ImageCanvasViewerViewModel.Zoom;
+
+            return model;
+        }
+    }
+}
